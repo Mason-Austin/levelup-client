ca8a383075c90813be18c03bf035e98af18b9e0d
Mason Austin <mason47austin@gmail.com>
2023-11-28 19:25:28 -0600

    finished form for games, intoduced functionality for adding games, created page for the game form, and added the create btn

--- pages/games/home.js
+++ pages/games/home.js
@@ -1,8 +1,11 @@
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import GameCard from '../../components/game/GameCard';
 import { getGames } from '../../utils/data/gameData';
 
 function Home() {
+  const router = useRouter();
   const [games, setGames] = useState([]);
 
   useEffect(() => {
@@ -12,6 +15,13 @@ function Home() {
   return (
     <article className="games">
       <h1>Games</h1>
+      <Button
+        onClick={() => {
+          router.push('/games/new');
+        }}
+      >
+        Register New Game
+      </Button>
       {games.map((game) => (
         <section key={`game--${game.id}`} className="game">
           <GameCard title={game.title} maker={game.maker} numberOfPlayers={game.number_of_players} skillLevel={game.skill_level} />
