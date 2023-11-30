211174af451d18bf146be0e91eab153105a5b9d7
Mason Austin <mason47austin@gmail.com>
2023-11-29 18:34:44 -0600

    created PUT promise for editing events

--- utils/data/eventData.js
+++ utils/data/eventData.js
@@ -19,5 +19,17 @@ const createEvent = (event) => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
+const updateEvent = (updatedEvent, eventId) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/events/${eventId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedEvent),
+  })
+    .then((data) => resolve(data))
+    .catch((error) => reject(error));
+});
+
 // eslint-disable-next-line import/prefer-default-export
-export { getEvents, createEvent };
+export { getEvents, createEvent, updateEvent };
