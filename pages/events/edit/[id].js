59b63c87246b70e03c9b3ff7e3371a4a9a59ffdd
Mason Austin <mason47austin@gmail.com>
2023-11-29 19:02:09 -0600

    created react route to render a the event form with the event to edit

---
+++ pages/events/edit/[id].js
@@ -0,0 +1,25 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import EventForm from '../../../components/event/EventForm';
+import { useAuth } from '../../../utils/context/authContext';
+import { getSingleEvent } from '../../../utils/data/eventData';
+
+const EditEvent = () => {
+  const router = useRouter();
+  const { user } = useAuth();
+  const { id } = router.query;
+  const [editEvent, setEditEvent] = useState();
+
+  useEffect(() => {
+    getSingleEvent(id).then(setEditEvent);
+  }, [id]);
+
+  return (
+    <div>
+      <h2>Register New Event</h2>
+      <EventForm user={user} initialEvent={editEvent} />
+    </div>
+  );
+};
+
+export default EditEvent;
