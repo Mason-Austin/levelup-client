924364e7a6e20b209a5d781f8da070ddc6a897d7
Mason Austin <mason47austin@gmail.com>
2023-12-02 10:41:03 -0600

    added deleteSingleEvent promise

--- utils/data/eventData.js
+++ utils/data/eventData.js
@@ -1,5 +1,16 @@
 import { clientCredentials } from '../client';
 
+const deleteSingleEvent = (eventId) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/events/${eventId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(resolve)
+    .catch(reject);
+});
+
 const getEvents = () => new Promise((resolve, reject) => {
   fetch(`${clientCredentials.databaseURL}/events`)
     .then((response) => response.json())
@@ -45,5 +56,5 @@ const getSingleEvent = (eventId) => new Promise((resolve, reject) => {
 
 // eslint-disable-next-line import/prefer-default-export
 export {
-  getEvents, createEvent, updateEvent, getSingleEvent,
+  getEvents, createEvent, updateEvent, getSingleEvent, deleteSingleEvent,
 };
