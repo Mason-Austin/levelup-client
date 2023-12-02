209a4e9e9051485c0390771d0da51fc6f0e13819
Mason Austin <mason47austin@gmail.com>
2023-12-02 10:39:29 -0600

    finished adding delete functionality to GameCard

--- components/game/GameCard.js
+++ components/game/GameCard.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { useRouter } from 'next/router';
 import { Card, Button } from 'react-bootstrap';
+import { deleteSingleGame } from '../../utils/data/gameData';
 
 const GameCard = ({
   id,
@@ -9,8 +10,15 @@ const GameCard = ({
   maker,
   numberOfPlayers,
   skillLevel,
+  onUpdate,
 }) => {
   const router = useRouter();
+
+  const deleteThisGame = () => {
+    if (window.confirm(`Delete ${title}?`)) {
+      deleteSingleGame(id).then(() => onUpdate());
+    }
+  };
   return (
     <Card className="text-center">
       <Card.Header>{title}</Card.Header>
@@ -20,12 +28,11 @@ const GameCard = ({
       </Card.Body>
       <Card.Footer className="text-muted">
         Skill Level: {skillLevel}
-        <Button
-          onClick={() => {
-            router.push(`/games/edit/${id}`);
-          }}
-        >
+        <Button onClick={() => { router.push(`/games/edit/${id}`); }}>
           Edit Game
+        </Button>
+        <Button className="btn btn-danger" onClick={deleteThisGame}>
+          Delete Game
         </Button>
       </Card.Footer>
     </Card>
@@ -38,6 +45,7 @@ GameCard.propTypes = {
   maker: PropTypes.string.isRequired,
   numberOfPlayers: PropTypes.number.isRequired,
   skillLevel: PropTypes.number.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
 
 export default GameCard;
